2a4bbd5f6766b371e9bc0c619dc6dc108f907c6a
Fonta97 <fontana.alessandro97@gmail.com>
2024-05-02 13:10:33 +0200

    Aggiungere i file di progetto.

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace GestoreEvento
+{
+    public class Evento
+    {
+
+        private string _titolo;
+        private DateTime _data;
+        private int _capienzaEvento;
+        private int _numeroPostiPrenotati;
+
+        public string Titolo
+        {
+            get
+            {
+                return _titolo;
+            }
+            set
+            {
+                //se il titolo non è una stringa vuota o spazi bianchi
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    _titolo = value;
+                }
+                else
+                {
+                    throw new Exception("Il titolo non può essere vuoto.");
+                }
+            }
+        }
+        public DateTime Data
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                //se la date è minore di oggi
+                if (value >= DateTime.Now)
+                {
+                    _data = value;
+                }
+                else
+                {
+                    throw new Exception("La data non può essere al passato");
+                }
+            }
+        }
+        public int CapienzaEvento
+        {
+            get
+            {
+                return _capienzaEvento;
+            }
+            private set
+            {
+
+                //se la capienza è maggiore di 0
+                if (value > 0)
+                {
+                    _capienzaEvento = value;
+                }
+                else
+                {
+                    throw new Exception("La capienza non può essere inferiore a 0");
+                }
+            }
+        }
+        public int NumeroPostiPrenotati { get; private set; }
+
+
+        public Evento(string titolo, DateTime date, int capienzaEvento)
+        {
+            Titolo = titolo;
+            Data = date;
+            CapienzaEvento = capienzaEvento;
+            NumeroPostiPrenotati = 0;
+        }
+
+
+        public int PrenotaPosti(int postiDaPrenotare)
+        {
+            // se l'evento è già passato
+            if (Data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato.");
+            }
+
+            //se il numero di posti è maggiore della capienza o i posti da prenotare sono maggiori della capienza
+            else if (NumeroPostiPrenotati > CapienzaEvento || CapienzaEvento < postiDaPrenotare)
+            {
+                throw new Exception("L'evento non ha più posti a disposizione.");
+            }
+
+            return NumeroPostiPrenotati += postiDaPrenotare;
+        }
+
+        public int DisdisciPosti(int postiDaCancellare)
+        {
+
+            if (Data < DateTime.Now)
+            {
+                throw new Exception("L'evento è già passato.");
+            }
+
+            if (NumeroPostiPrenotati >= postiDaCancellare)
+            {
+                NumeroPostiPrenotati -= postiDaCancellare;
+            }
+            else
+            {
+                throw new Exception("Non ci sono abbastanza posti prenotati da disdire.");
+            }
+
+
+            return NumeroPostiPrenotati -= postiDaCancellare;
+
+        }
+
+        public override string ToString()
+        {
+            string data = Data.ToString("dd/MM/yyyy");
+
+            return $"{data} - {Titolo}";
+        }
+
+    }
---
+++ csharp-gestore-eventi/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace GestoreEvento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.WriteLine("Inserisci i dettagli del nuovo evento:");
+
+
+                //titolo
+                Console.Write("Inserisci il nome dell'evento: ");
+                string titolo = Console.ReadLine();
+
+
+                //data
+                Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+                DateTime data = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+
+
+                //capienza
+                Console.Write("Inserisci il numero di posti totali: ");
+                int capienzaEvento = int.Parse(Console.ReadLine());
+
+
+
+                Evento nuovoEvento = new Evento(titolo, data, capienzaEvento);
+
+                Console.WriteLine();
+                Console.Write("Vuoi effettuare delle prenotazioni? (si/no): ");
+                string risposta = Console.ReadLine();
+
+                Console.WriteLine();
+                if (risposta == "si")
+                {
+                    //posti prenotati
+                    Console.WriteLine("Quanti posti desideri prenotare? ");
+                    int numeroPostiCheSiVoglionoPrenotare = int.Parse(Console.ReadLine());
+
+                    // effettua le prenotazioni utilizzando il metodo PrenotaPosti
+                    int postiPrenotati = nuovoEvento.PrenotaPosti(numeroPostiCheSiVoglionoPrenotare);
+
+                    Console.WriteLine();
+                    Console.WriteLine($"Numero di posti prenotati: {postiPrenotati}");
+                    Console.WriteLine($"Numero di posti disponibili: {nuovoEvento.CapienzaEvento - postiPrenotati}");
+                }
+
+                Console.WriteLine();
+                while (risposta == "si")
+                {
+                    Console.WriteLine("Vuoi disdire dei posti? (si/no): ");
+                    risposta = Console.ReadLine();
+
+                    if (risposta == "si")
+                    {
+                        Console.WriteLine($"Ok, va bene!");
+                        //posti da disdire
+                        Console.WriteLine($"Indica il numero dei posti che vuoi disdire: ");
+                        int numeroPostiCheSiVoglionoDisdire = int.Parse(Console.ReadLine());
+
+                        // effettua le disdette utilizzando il metodo DisdisciPosti
+                        int postiCancellati = nuovoEvento.DisdisciPosti(numeroPostiCheSiVoglionoDisdire);
+
+                        Console.WriteLine();
+                        Console.WriteLine($"Numero di posti prenotati: {nuovoEvento.NumeroPostiPrenotati}");
+                        Console.WriteLine($"Numero di posti disponibili: {nuovoEvento.CapienzaEvento - nuovoEvento.NumeroPostiPrenotati}");
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Si è verificato un errore: {e}");
+            }
+        }
+    }
+}
